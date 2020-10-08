d5c79db39ea19edf52b687afb2fcfb778893848e
erika kettleson <erikakettleson@gmail.com>
2020-10-08 14:04:32 -0700

    initialize react app marco

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 import logo from './logo.svg';
 import './App.css';
+import 'react-calendar/dist/Calendar.css';
 
-function App() {
+export default function Results() {
+  const [calDate, setCalDate] = useState(new Date())
+
+  function onChange (catDate) {
+    setCalDate(calDate)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="result-calendar">
+      <Calendar onChange={onChange} value={calDate} />
     </div>
-  );
+  )
 }
-
-export default App;
